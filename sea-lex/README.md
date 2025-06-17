4b1e644126d2527ad9b9ea8b40afd0d4658df1dc
Cayden Lund <caydenlund@gmail.com>
2025-06-17 17:50:20 -0600

    sea-lex setup api proof of concept

--- sea-lex/README.md
+++ sea-lex/README.md
@@ -1,5 +1,65 @@
 # `sea-lex`
 
-SeaFlow lex component.
+SeaFlow lexer component.
 
 Part of the [SeaFlow](https://github.com/caydenlund/seaflow) compiler toolkit.
+
+The lexer is one of the less-interesting components of the compiler, so most of my attention will be spent elsewhere.
+This means that it may be less efficient than other lexing crates, but because lexing is a small fraction of the overall work of the compiler, inefficiency is not a large concern here.
+I intend to come back to this and implement a state machine generator that will be much more efficient.
+
+## Design Overview
+
+Given a source program, the lexer identifies the most primitive building blocks ("tokens") that will be parsed into an Abstract Syntax Tree.
+These tokens include things like keywords, punctuation, and identifiers.
+
+The overall lexer is composed of pattern matchers, ordered by priority.
+Each pattern matcher will read from the source source program at a given position, and determine whether to emit a valid token type from that position.
+
+## API Overview
+
+Implement the `TokenType` trait for an `enum` of your token types by defining the ordered pattern matchers.
+Each pattern matcher is a pair of a `TokenCreator` and a regular or expression or string literal to match.
+This trait will make it possible to construct a new `Lexer` for this set of token types.
+
+```rust
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Token {
+    Integer(i64),
+    Identifier(String),
+    LParen,
+    RParen,
+    Asterisk,
+    Slash,
+    Plus,
+    Minus,
+}
+
+impl TokenType for Token {
+    fn matchers() -> Vec<Matcher<Self>> {
+        vec![
+            // Integer literals
+            (
+                TokenCreator::Fn(Box::new(|c| Self::Integer(c.parse().unwrap()))),
+                Regex::new(r"^\d+").unwrap(),
+            )
+                .into(),
+            // Variable names
+            (
+                TokenCreator::Fn(Box::new(|c| Self::Identifier(c.to_string()))),
+                Regex::new(r"^[a-zA-Z_]+").unwrap(),
+            )
+                .into(),
+            // Punctuation
+            (Self::LParen, "(").into(),
+            (Self::RParen, ")").into(),
+            (Self::Asterisk, "*").into(),
+            (Self::Slash, "/").into(),
+            (Self::Plus, "+").into(),
+            (Self::Minus, "-").into(),
+            // Ignore whitespace
+            (TokenCreator::None, Regex::new(r"^\s+").unwrap()).into(),
+        ]
+    }
+}
+```
--- sea-lex/src/lib.rs
+++ sea-lex/src/lib.rs
@@ -1,4 +1,4 @@
-//! sea-lex - SeaFlow lexer component
+//! sea-lex - seaflow lexer component
 
 #![warn(
     clippy::all,
@@ -9,3 +9,129 @@
     missing_docs,
     rustdoc::all
 )]
+
+use regex::Regex;
+
+pub enum TokenCreator<T> {
+    Cloned(T),
+    Fn(Box<dyn Fn(&str) -> T + Send + Sync + 'static>),
+    None,
+}
+
+impl<T> TokenCreator<T> {
+    pub fn create(&self, matched_str: &str) -> Option<T>
+    where
+        T: Clone, // `T` must be cloneable for the `Cloned` variant
+    {
+        match self {
+            TokenCreator::Cloned(token) => Some(token.clone()),
+            TokenCreator::Fn(f) => Some(f(matched_str)),
+            TokenCreator::None => None,
+        }
+    }
+}
+
+impl<T> From<T> for TokenCreator<T>
+where
+    T: Clone,
+{
+    fn from(token: T) -> Self {
+        TokenCreator::Cloned(token)
+    }
+}
+
+pub enum MatcherPattern {
+    Literal(String),
+    Regex(Regex),
+}
+
+impl MatcherPattern {
+    pub fn matches(&self, text: &str) -> Option<usize> {
+        match self {
+            MatcherPattern::Literal(s) => text.starts_with(s).then_some(s.len()),
+            MatcherPattern::Regex(r) => r.find(text).map(|m| m.end()),
+        }
+    }
+}
+
+// Implement From for convenience
+impl From<&str> for MatcherPattern {
+    fn from(s: &str) -> Self {
+        MatcherPattern::Literal(s.to_string())
+    }
+}
+
+impl From<Regex> for MatcherPattern {
+    fn from(regex: Regex) -> Self {
+        MatcherPattern::Regex(regex)
+    }
+}
+
+pub struct Matcher<T> {
+    creator: TokenCreator<T>,
+    pattern: MatcherPattern,
+}
+
+impl<T, C> From<(C, Regex)> for Matcher<T>
+where
+    C: Into<TokenCreator<T>>,
+{
+    fn from((creator, regex): (C, Regex)) -> Self {
+        Self {
+            creator: creator.into(),
+            pattern: MatcherPattern::Regex(regex),
+        }
+    }
+}
+
+impl<T, C> From<(C, &str)> for Matcher<T>
+where
+    C: Into<TokenCreator<T>>,
+{
+    fn from((creator, literal): (C, &str)) -> Self {
+        Self {
+            creator: creator.into(),
+            pattern: MatcherPattern::Literal(literal.to_string()),
+        }
+    }
+}
+
+pub trait TokenType: Sized + Clone {
+    fn matchers() -> Vec<Matcher<Self>>;
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Token {
+    Integer(i64),
+    Identifier(String),
+    LParen,
+    RParen,
+    Asterisk,
+    Slash,
+    Plus,
+    Minus,
+}
+
+impl TokenType for Token {
+    fn matchers() -> Vec<Matcher<Self>> {
+        vec![
+            (
+                TokenCreator::Fn(Box::new(|c| Self::Integer(c.parse().unwrap()))),
+                Regex::new(r"^\d+").unwrap(),
+            )
+                .into(),
+            (
+                TokenCreator::Fn(Box::new(|c| Self::Identifier(c.to_string()))),
+                Regex::new(r"^[a-zA-Z_]+").unwrap(),
+            )
+                .into(),
+            (Self::LParen, "(").into(),
+            (Self::RParen, ")").into(),
+            (Self::Asterisk, "*").into(),
+            (Self::Slash, "/").into(),
+            (Self::Plus, "+").into(),
+            (Self::Minus, "-").into(),
+            (TokenCreator::None, Regex::new(r"^\s+").unwrap()).into(),
+        ]
+    }
+}
